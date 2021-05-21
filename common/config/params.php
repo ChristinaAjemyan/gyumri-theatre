e248f78a27c34be8a94c8d5da0f71a6c29316919
root <root@vmi103709.contaboserver.net>
2021-05-21 12:15:21 +0200

    server config file

--- common/config/params.php
+++ common/config/params.php
@@ -6,8 +6,8 @@ return [
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
-    'backend-url' => 'http://admin.gyumritheatre.am',
-    'front-url' => 'http://demo.gyumritheatre.am',
-/*   'backend-url' => 'http://theatre.loc',
+    'backend-url' => 'https://admin.gyumritheatre.am',
+    'front-url' => 'https://gyumritheatre.am/gyumri-theatre/frontend/web',
+/*    'backend-url' => 'http://theatre.loc',
     'front-url' => 'http://front.loc',*/
 ];
